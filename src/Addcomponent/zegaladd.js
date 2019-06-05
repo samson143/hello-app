5265bdbae06adf0c9165a0f262469d38a61a76e5
samson143 <samson54@gmail.com>
2019-06-05 14:33:57 +0545

    add/zegaladd

---
+++ src/Addcomponent/zegaladd.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+
+class Zegaladd extends React.Component {
+    render(){
+        return(
+            <div>
+                This is Zegaladd.
+            </div>
+        )
+    }
+}
+
+export default Zegaladd;
